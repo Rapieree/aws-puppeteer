acaa3417a17d1159a8c8c6b5ba6086ce4fc3b8b6
Sergey <serega.mironov11@gmail.com>
2023-03-02 00:22:37 +0300

    Add query params

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -4,6 +4,13 @@ const chrome = require(`chrome-aws-lambda`);
 const puppeteer = require(`puppeteer-core`);
 
 export default async (req, res) => {
+  const {url, selector} = req.query;
+
+  if (!url || !selector) {
+    res.status(200).send(`Missed params "url" and "selector"`);
+    return;
+  }
+
   const browser = await puppeteer.launch(
       process.env.IS_PROD === `true`
         ? {
@@ -14,17 +21,19 @@ export default async (req, res) => {
         : {}
   );
 
-  const page = await browser.newPage();
-  await page.setUserAgent(`Opera/9.80 (J2ME/MIDP; Opera Mini/5.1.21214/28.2725; U; ru) Presto/2.8.119 Version/11.10`);
-  await page.goto(`https://dzen.ru`);
+  try {
+    const page = await browser.newPage();
+    await page.setUserAgent(`Opera/9.80 (J2ME/MIDP; Opera Mini/5.1.21214/28.2725; U; ru) Presto/2.8.119 Version/11.10`);
+    await page.goto(decodeURIComponent(url));
 
-  let content = await page.content();
-  const jsdom = new JSDOM(content);
-  let title = jsdom.window.document.querySelector(`title`);
+    let content = await page.content();
+    const jsdom = new JSDOM(content);
+    let elem = jsdom.window.document.querySelector(decodeURIComponent(selector));
 
-  await browser.close();
+    await browser.close();
 
-  res.statusCode = 200;
-  res.setHeader(`Content-Type`, `application/json`);
-  res.end(JSON.stringify({title: title.textContent}));
+    res.json({elem: elem.textContent});
+  } catch (err) {
+    res.status(400).json({error: err});
+  }
 };
